05b0bbabecb5430cf75b25ddc982b35ad69360e0
Carlos Lucas <clucas@hotelbeds.com>
2019-10-11 11:35:42 +0200

    DSL WIP

--- src/dsl.py
+++ src/dsl.py
@@ -1,4 +1,8 @@
-colors = {
+import time
+from leds import leds
+
+
+COLORS = {
     'X': (0, 0, 0),
     'R': (255, 0, 0),
     'G': (0, 255, 0),
@@ -9,3 +13,35 @@ colors = {
     'P': (255, 0, 255)
 }
 
+
+def wait_operation(param):
+    milis = int(param)
+    time.sleep(milis/1000.0)
+
+
+def led_operation(param):
+    for i, c in enumerate(param.upper()):
+        leds[i] = COLORS[c]
+    leds.show()
+
+
+def tag_operation(param):
+    pass
+
+
+def jump_operation(param):
+    pass
+
+
+OPERATIONS = {
+    "WAIT": wait_operation,
+    "LED": led_operation,
+    "TAG": tag_operation,
+    "JUMP": jump_operation
+}
+
+
+class Program:
+
+    def __init__(self):
+        pass
