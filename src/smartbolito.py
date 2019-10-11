9991e3423e2201dd7ec946790a6a5f01c98bb097
Carlos Lucas <clucas@hotelbeds.com>
2019-10-11 12:56:28 +0200

    running program

--- src/smartbolito.py
+++ src/smartbolito.py
@@ -1,6 +1,22 @@
 from slack import RTMClient
+from dsl import Program
 import leds
 import os
+
+running_program = None
+
+
+def stop_running_program():
+    global running_program
+    if running_program:
+        running_program.stop()
+
+
+def run_program(raw_program):
+    global running_program
+    stop_running_program()
+    running_program = Program(raw_program)
+    running_program.start()
 
 
 @RTMClient.run_on(event='message')
@@ -13,19 +29,26 @@ def handle_event(**payload):
             file_id = data.get('files')[0]['id']
             file_content = web_client.files_info(file=file_id).data['content']
 
-            return #TODO implement file interpretation
+            run_program(file_content)
 
         elif data.get('text'):
             msg = data.get('text').strip().lower()
             if msg == 'on':
                 leds.turn_on()
+
             elif msg == 'off':
+                stop_running_program()
                 leds.turn_off()
+
             elif msg.startswith('color'):
                 colors = msg.split()[1:]
+                stop_running_program()
                 leds.set_color([int(c) for c in colors])
+
             elif msg.startswith('random-color'):
+                stop_running_program()
                 leds.set_random_color()
+
             elif msg.startswith('party-hard'):
                 leds.set_random_color_two()
 
@@ -34,7 +57,7 @@ def handle_event(**payload):
 
         web_client.reactions_add(
             channel=channel_id,
-            name="thumbsup",
+            name="heavy_check_mark",
             timestamp=event_timestamp
         )
 
